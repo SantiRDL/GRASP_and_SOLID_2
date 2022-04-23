df085acb2e13e0cbbc25194c20d2ea514a4401e9
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2022-04-23 11:07:31 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -22,7 +22,7 @@ namespace Full_GRASP_And_SOLID
             PopulateCatalogs();
 
             Recipe recipe = new Recipe();
-            recipe.FinalProduct = GetProduct("Café con leche");
+            recipe.FinalProduct = GetProduct("Café con leche ");
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
             ConsolePrinter.Imprimir(recipe.GetStringRecipe());
